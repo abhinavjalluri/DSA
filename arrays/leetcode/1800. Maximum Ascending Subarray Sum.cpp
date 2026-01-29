11d877d58497ee34e00939f300491bea31a28084
abhinavjalluri <abhirunner2002@gmail.com>
2026-01-29 23:06:02 +0530

    Add solution for maximum ascending subarray sum problem

---
+++ arrays/leetcode/1800. Maximum Ascending Subarray Sum.cpp
@@ -0,0 +1,46 @@
+// Given an array of positive integers nums, return the maximum possible sum of an strictly increasing subarray in nums.
+
+// A subarray is defined as a contiguous sequence of numbers in an array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [10,20,30,5,10,50]
+// Output: 65
+// Explanation: [5,10,50] is the ascending subarray with the maximum sum of 65.
+// Example 2:
+
+// Input: nums = [10,20,30,40,50]
+// Output: 150
+// Explanation: [10,20,30,40,50] is the ascending subarray with the maximum sum of 150.
+// Example 3:
+
+// Input: nums = [12,17,15,13,10,11,12]
+// Output: 33
+// Explanation: [10,11,12] is the ascending subarray with the maximum sum of 33.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 100
+// 1 <= nums[i] <= 100
+
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int n = nums.size();
+        int sum = nums[0];
+        int maxi = INT_MIN;
+        for(int i = 1;i<n;i++){
+            if(nums[i] > nums[i-1]){
+                sum += nums[i];
+            }else{
+                maxi = max(maxi,sum);
+                sum = nums[i];
+            }
+        }
+        return max(maxi,sum);
+        
+    }
+};
