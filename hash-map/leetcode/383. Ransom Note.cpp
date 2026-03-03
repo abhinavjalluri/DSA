4828116d9c4efb58c2872246d546b775f293385e
abhinavjalluri <abhirunner2002@gmail.com>
2026-03-03 13:41:29 +0530

    Add implementations for various hash map problems including Two Sum, Longest Consecutive Sequence, Isomorphic Strings, Contains Duplicate II, Valid Anagram, Word Pattern, Ransom Note, and Group Anagrams

---
+++ hash-map/leetcode/383. Ransom Note.cpp
@@ -0,0 +1,43 @@
+// Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+// Each letter in magazine can only be used once in ransomNote.
+
+ 
+
+// Example 1:
+
+// Input: ransomNote = "a", magazine = "b"
+// Output: false
+// Example 2:
+
+// Input: ransomNote = "aa", magazine = "ab"
+// Output: false
+// Example 3:
+
+// Input: ransomNote = "aa", magazine = "aab"
+// Output: true
+ 
+
+// Constraints:
+
+// 1 <= ransomNote.length, magazine.length <= 105
+// ransomNote and magazine consist of lowercase English letters.
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        if(ransomNote.size() > magazine.size()){
+            return false;
+        }
+        vector<int>count(26,0);
+        for(int i = 0 ; i < magazine.size() ; i++){
+            count[magazine[i] - 'a']++;
+        }
+        for(int i = 0 ; i < ransomNote.size() ; i++){
+            if(count[ransomNote[i] - 'a']-- <= 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
