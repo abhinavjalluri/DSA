61742bffeb33e90765a7005b5f9ee08528461699
abhinavjalluri <abhirunner2002@gmail.com>
2026-07-19 23:39:15 +0530

    Add implementation for checking anagrams using unordered_map and add new solution for counting elements with maximum frequency

--- coding/hash-map/leetcode/242. Valid Anagram.cpp
+++ coding/hash-map/leetcode/242. Valid Anagram.cpp
@@ -41,3 +41,25 @@ public:
         return true;
     }
 };
+
+// Another approach using unordered_map
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        unordered_map<char,int>map;
+        for(int i = 0 ; i < s.size(); i++){
+            map[s[i]]++;
+        }
+        for(int i = 0 ; i < t.size() ; i++){
+            map[t[i]]--;
+        }
+        for(auto i : map){
+            if(i.second != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
